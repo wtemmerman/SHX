d74a323dcbec5594894dc4f5c23604608a663316
wiL <wtemmerman@epsi.fr>
2011-02-17 23:46:48 +0100

    add Constants.hpp

---
+++ Constants.hpp
@@ -0,0 +1,18 @@
+#ifndef CONSTANTS_HPP
+#define	CONSTANTS_HPP
+
+#include "SDL/SDL.h"
+#include "SDL/SDL_image.h"
+
+const int SCREEN_WIDTH=640;
+const int SCREEN_HEIGHT=480;
+const int SCREEN_BPP=32;
+
+//Les dimensions du vaisseau joueur (l'image qu'on va deplacer)
+const int PLAYER_WIDTH = 50;
+const int PLAYER_HEIGHT = 50;
+
+const unsigned int wantedfps = 60;
+
+#endif	/* CONSTANTS_HPP */
+
